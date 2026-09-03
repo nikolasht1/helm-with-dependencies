cce382b2c450d253c063484ed605303889fcfd30
nikolasht1 <nikolasht@outlook.com>
2026-09-03 15:37:14 +0300

    redacted create-chart

--- create-chart.sh
+++ create-chart.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
+cd helm-with-dependencies/
+
 helm repo update
 
 kubectl create namespace monitoring
 
-helm install k-prom-black prometheus-community/prometheus-blackbox-exporter --namespace monitoring -f blackbox-values.yaml
-
-helm upgrade --install k-prom-black prometheus-community/prometheus-blackbox-exporter -n monitoring -f blackbox-values.yaml
-
-helm install k-prom-stack prometheus-community/kube-prometheus-stack --namespace monitoring -f kube-prometheus-values.yaml
+helm install k-prom-black ./universe-test/ --namespace monitoring
 
 export $(grep -v '^#' .env | xargs)
 
-helm upgrade --install k-prom-stack prometheus-community/kube-prometheus-stack -n monitoring -f kube-prometheus-values.yaml \
+helm upgrade --install k-prom-black ./universe-test/ -n monitoring -f ./universe-test/custom-values.yaml \
 	--set grafana.adminUser=$GRAFANA_USER \
 	--set grafana.adminPassword=$GRAFANA_PASSWORD \
 	--set grafana.dashboards.default.blackbox-exporter-http-prober.gnetId=$GRAFANA_DASHBOARD
-
-kubectl apply -f google-probe.yaml
-
-#sleep 40
-
-#kubectl port-forward svc/k-prom-stack-grafana 3000:80 -n monitoring
