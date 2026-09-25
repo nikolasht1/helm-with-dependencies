8cc629c41ed36f13504ef41ad2403bca54431e42
nikolasht1 <nikolasht@outlook.com>
2026-09-25 18:17:42 +0300

    added dashboard to configmap

--- create-chart.sh
+++ create-chart.sh
@@ -13,4 +13,4 @@ export $(grep -v '^#' .env | xargs)
 helm upgrade --install k-prom-black ./universe-test/ -n monitoring -f ./universe-test/custom-values.yaml \
 	--set grafana.adminUser=$GRAFANA_USER \
 	--set grafana.adminPassword=$GRAFANA_PASSWORD \
-	--set grafana.dashboards.default.blackbox-exporter-http-prober.gnetId=$GRAFANA_DASHBOARD
+	#--set grafana.dashboards.default.Blackbox exporter HTTP prober dashboard.gnetId=$GRAFANA_DASHBOARD
